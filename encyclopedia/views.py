e3b4280b4ad7f5c4c4e25463aafbce4f0d0e7285
JesMas <jesse.mastenrbroek@kpnmail.nl>
2021-12-01 11:23:49 +0100

    added search and error page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,7 +9,24 @@ def index(request):
         "entries": util.list_entries()
     })
 def entry(request, TITLE):
+    if util.get_entry(TITLE) == None:
+        return render(request, "encyclopedia/error.html")
     return render(request, "encyclopedia/entry.html", {
-        "entry": markdown2.markdown(util.get_entry(TITLE))
+        "entry": markdown2.markdown(util.get_entry(TITLE)),
+        "title": TITLE
     })
-
+def search(request):
+    results = []
+    entries = util.list_entries()
+    query = request.GET.get('q')
+    for entry in entries:
+        if query == entry:
+            return render(request, "encyclopedia/entry.html", {
+                "entry": markdown2.markdown(util.get_entry(query)),
+                "title": query
+            })
+        if entry.find(query) != -1:
+            results.append(entry)
+    return render(request, "encyclopedia/search.html", {
+        "results": results
+    })
